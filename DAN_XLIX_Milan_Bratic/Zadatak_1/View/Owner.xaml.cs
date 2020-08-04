21a3781c4a3d2fe3fca081e738c2bea1963f8cb4
MilanBratic0512 <milan.bratic2@gmail.com>
2020-08-04 15:27:17 +0200

    create owner view

--- DAN_XLIX_Milan_Bratic/Zadatak_1/View/Owner.xaml.cs
+++ DAN_XLIX_Milan_Bratic/Zadatak_1/View/Owner.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Zadatak_1.Model;
 
 namespace Zadatak_1.View
 {
@@ -19,9 +20,58 @@ namespace Zadatak_1.View
     /// </summary>
     public partial class Owner : Window
     {
+        Zadatak_49Entities context = new Zadatak_49Entities();
         public Owner()
         {
             InitializeComponent();
+        }
+
+        private void BtnManager(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                tblManager manager = new tblManager();
+                manager.FullName = managerName.Text;
+                manager.DateOfBirth = DateTime.Parse(managerDateOfBirth.Text);
+                manager.Mail = managerMail.Text;
+                manager.HotelFloor = int.Parse(managerFloor.Text);
+                manager.Username = managerUsername.Text;
+                manager.MannagerPassword = managerPassword.Text;
+                manager.ProfessionalQualifications = qualifications.Text;
+                manager.Experience = int.Parse(experience.Text);
+                context.tblManagers.Add(manager);
+                context.SaveChanges();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Incorect entry");
+            }
+
+        }
+
+        private void BtnEmployer(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+                tblEmployer employer = new tblEmployer();
+                employer.Citizenship = citizenship.Text;
+                employer.DateOfBirth = DateTime.Parse(employerDateOfBirth.Text);
+                employer.EmployerPassword = employerPassword.Text;
+                employer.Engagement = engagment.Text;
+                employer.FullName = employerName.Text;
+                employer.Gender = gender.Text;
+                employer.HotelFloor = int.Parse(employerFloor.Text);
+                employer.Mail = employerMail.Text;
+                employer.Salary = decimal.Parse(salary.Text);
+                employer.Username = employerUsername.Text;
+                context.tblEmployers.Add(employer);
+                context.SaveChanges();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Incorect entry");
+            }
         }
     }
 }
